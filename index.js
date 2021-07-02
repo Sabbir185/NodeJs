47e75d25dfa5f196e78d8fbd2975155306ed1dec
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-07-02 20:20:48 +0600

    environment setup and npm install -g win-node-env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // module dependencies
 const http = require('http');
-const {handleReqRes} = require('./helpers/handleReqRes')
+const {handleReqRes} = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
 
 // module scaffolding
 const app = {};
@@ -13,8 +14,8 @@ app.config = {
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`Server port listening ${app.config.port}`)
+    server.listen(environment.port, () => {
+        console.log(`Server port listening ${environment.port}`)
     })
 }
 
