5637fd0b6c19261a872c6e3be089d22018e71ea9
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-07-03 11:19:14 +0600

    file system CRUD done!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const http = require('http');
 const {handleReqRes} = require('./helpers/handleReqRes');
 const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // module scaffolding
 const app = {};
