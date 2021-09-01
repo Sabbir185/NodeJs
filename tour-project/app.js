335ac4701625ac456d2ed7342d4a7ddceb0b3ff6
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-09-02 00:05:27 +0600

    crud operation by using file system data done

--- tour-project/app.js
+++ tour-project/app.js
@@ -12,14 +12,96 @@ const tourData = JSON.parse(fs.readFileSync(path.join(`${__dirname}`, 'data/tour
 const app = express();
 
 // middleware
-
+app.use(express.json())
 
 // router
+// get all tour
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
         totalData: tourData.length,
         tourData
+    })
+})
+
+// search by id
+app.get('/api/v1/tours/:id', (req, res) => {
+    const tourID = req.params.id * 1;
+    const tour = tourData.find(el => el.id === tourID);
+
+    // if(tourID > tourData.length-1) 
+    if (!tour) {
+        res.status(400).json({
+            status: 'Data not found!'
+        })
+    }
+
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            tour
+        }
+    });
+})
+
+
+// post tour
+app.post('/api/v1/tours', (req, res) => {
+    const newID = tourData[tourData.length - 1].id + 1;
+    const newData = Object.assign({ id: newID }, req.body);
+    tourData.push(newData);
+
+    fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tourData), err => {
+        res.status(201).json({
+            status: 'success',
+            newData
+        });
+    })
+
+})
+
+// update tour
+app.patch('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    if (id > tourData.length - 1) {
+        res.status(404).json({
+            status: 'failed'
+        })
+    }
+
+    const tour = tourData.find(el => el.id === id);
+
+    tour.name = req.body.name;
+
+    tourData.splice(id, 1);
+
+    tourData[id] = tour; // update or insert that index
+
+    fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tourData), err => {
+        res.status(201).json({
+            status: 'updated successful',
+            tour
+        });
+    })
+})
+
+
+// delete tour
+app.delete('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    if (id > tourData.length - 1) {
+        res.status(404).json({
+            status: 'failed'
+        })
+    }
+
+    tourData.splice(id, 1);
+
+
+    fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tourData), err => {
+        res.status(204).json({
+            status: 'deleted successful',
+        });
     })
 })
 
