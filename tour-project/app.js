6c20d23bc64b96a3894b5627a009ce707dd7cb01
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-09-02 12:16:04 +0600

    static middleware added

--- tour-project/app.js
+++ tour-project/app.js
@@ -11,6 +11,7 @@ const app = express();
 
 // middleware
 app.use(express.json())
+app.use(express.static(`${__dirname}/public/`));
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
