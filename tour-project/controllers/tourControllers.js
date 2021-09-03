e52a8e5ece2ed5093d8471479430cc5999cdfc65
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-09-03 15:01:39 +0600

    get all data and find by id api added

--- tour-project/controllers/tourControllers.js
+++ tour-project/controllers/tourControllers.js
@@ -2,10 +2,40 @@
 const Tour = require("../models/tourModel");
 
 // get all tour
-exports.getTours = (req, res) => {};
+exports.getTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: "Success",
+      tours: {
+        data: tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail!",
+      message: err,
+    });
+  }
+};
 
 // search by id
-exports.getTour = (req, res) => {};
+exports.getTour = async (req, res) => {
+  try {
+      const tour = await Tour.findById(req.params.id);
+      res.status(200).json({
+          status: 'success',
+          tour: {
+              data: tour
+          }
+      })
+  } catch (err) {
+    res.status(404).json({
+        status: "fail!",
+        message: err,
+      });
+  }
+};
 
 // post tour
 exports.postTour = async (req, res) => {
@@ -20,7 +50,7 @@ exports.postTour = async (req, res) => {
         tour: newTour,
       },
     });
-  } catch(err) {
+  } catch (err) {
     res.status(400).json({
       status: "fail!",
       message: err,
