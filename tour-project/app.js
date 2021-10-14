ff5d4d26567629ed44742e517cb8e421d1d4e8ae
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-10-14 19:59:25 +0600

    error handling added

--- tour-project/app.js
+++ tour-project/app.js
@@ -30,4 +30,31 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', UserRouter);
 
 
+// handle unwanted routes
+app.use('*', (req, res, next) => {
+    // res.status(404).json({
+    //     status: 'failed',
+    //     message: `Requested ${req.originalUrl} not found!`
+    // });
+
+    // checking global error by through new Error()
+    const err = new Error(`Requested ${req.originalUrl} not found!`);
+    err.statusCode = 400;
+    err.status = 'failed';
+    next(err);
+});
+
+
+// global error handling
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
+
+
 module.exports = app;
