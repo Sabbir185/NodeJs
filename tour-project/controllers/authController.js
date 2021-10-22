c99d81f2450a47f1fae38b2ac62bb435db8d69fa
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-10-22 12:03:29 +0600

    forgot password email send done using nodemailer

--- tour-project/controllers/authController.js
+++ tour-project/controllers/authController.js
@@ -3,6 +3,7 @@ const catchAsync = require('../utilities/catchAsync');
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util')
 const AppError = require('../utilities/appError');
+const sendEmail = require('../utilities/email');
 
 
 // generate token
@@ -101,3 +102,45 @@ exports.restrictTo = ( ...roles ) => {
         next();
     }
 }
+
+
+// forgot password
+exports.forgotPassword = catchAsync(async(req, res, next) => {
+    // 1. get posted email
+    const user = await User.findOne({email: req.body.email});
+    if(!user) {
+        return next(new AppError('There is no user with this email', 404));
+    };
+
+    // 2. generate the random reset token
+    const resetToken = user.createPasswordResetToken();
+    await user.save({ validateBeforeSave: false });
+
+    // 3. send it to user's email
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+
+    const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to : ${resetURL}.\nif you didn't forget your password, please ignore it.`
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Your password reset token <valid for 10 min>',
+            message
+        })
+    
+        res.status(200).json({
+            status: 'success',
+            message: 'Token sent to email'
+        })
+
+    } catch (error) {
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({ validateBeforeSave: false });
+
+        return next(new AppError('There was an error sending the email, please try again!', 500));
+    };
+});
+
+// reset password
+exports.resetPassword = (req, res, next) => {};
--- tour-project/models/UserModel.js
+++ tour-project/models/UserModel.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -39,7 +40,9 @@ const userSchema = new mongoose.Schema({
             message: "Password confirmation is invalid"
         }
     },
-    passwordChangedAt: Date
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date
 });
 
 
@@ -71,6 +74,17 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
 
     // false means Not changed
     return false;
+}
+
+// create reset token
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+    console.log(resetToken,'   ', this.passwordResetToken)
+    return resetToken;
 }
 
 
