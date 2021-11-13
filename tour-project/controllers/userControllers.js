b891c4524f37a62917e7e349a9e9463a4404eec9
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-11-13 23:52:30 +0600

    user photo upload implemented using multer & sharp

--- tour-project/controllers/userControllers.js
+++ tour-project/controllers/userControllers.js
@@ -2,7 +2,50 @@ const AppError = require('../utilities/appError');
 const User = require('../models/UserModel');
 const catchAsync = require('../utilities/catchAsync');
 const factory = require('./handleRefactory');
+const multer = require('multer');
+const sharp = require('sharp');
 
+// const multerStorage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'public/img/users');
+//     },
+//     filename: (req, file, cb) => {
+//         const ext = file.mimetype.split('/')[1];
+//         cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//     }
+// })
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+    if(file.mimetype.startsWith('image')) {
+        cb(null, true)
+    }else{
+        cb(new AppError('Please upload only an image!', 400), false)
+    }
+}
+
+const upload = multer({ 
+    storage: multerStorage,
+    fileFilter: multerFilter
+});
+
+
+exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+    if(!req.file) return next();
+   
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+    sharp(req.file.buffer)
+        .resize(500, 500)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/users/${req.file.filename}`)
+    
+    next();
+}
 
 
 const filteredObj = (obj, ...allowedFields) => {
@@ -36,6 +79,7 @@ exports.getMe = (req, res, next) => {
 
 // update user self
 exports.updateMe = catchAsync(async(req, res, next) => {
+
     // 1) create error if user POSTed password data
     if(req.body.password || req.body.passwordConfirm) {
         return next(new AppError('This routes is not for password updates', 400));
@@ -43,6 +87,8 @@ exports.updateMe = catchAsync(async(req, res, next) => {
 
     // 2) update user document
     const filteredFields = filteredObj(req.body, 'name', 'email');
+    if(req.file) filteredFields.photo = req.file.filename;
+
     const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredFields, {new: true, runValidators: true});
 
     res.status(200).json({
--- tour-project/routes/userRoutes.js
+++ tour-project/routes/userRoutes.js
@@ -19,7 +19,7 @@ router.get('/me', userControllers.getMe, userControllers.getUser);
 
 router.patch('/updateMyPassword', authController.updateMyPassword);
 
-router.patch('/updateMe', userControllers.updateMe);
+router.patch('/updateMe', userControllers.uploadUserPhoto, userControllers.resizeUserPhoto, userControllers.updateMe);
 
 router.delete('/deleteMe', userControllers.deleteMe);
 
