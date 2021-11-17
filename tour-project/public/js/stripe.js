402ecdc28104ce26f4b4e0414471c37b267c9afb
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-11-17 20:01:20 +0600

    stripe client and server payment process done

---
+++ tour-project/public/js/stripe.js
@@ -0,0 +1,25 @@
+const stripe = Stripe('pk_test_51IeLQQJvn90Urdz7TTW8LBbHX1k1LUFWgveadGfsJKGdiI95E1m6DKTJW9NjGgSM58J5mtzhq3FbvTs9PzLFI0nk00j42bLZfM');
+
+const bookTour = tourId => {
+    fetch(`http://localhost:8080/api/v1/bookings/checkout-session/${tourId}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            // payment magic
+            stripe.redirectToCheckout({
+                sessionId: data.session.id
+            });
+
+        }).catch(err => {
+            console.log(err);
+            alert(err.message)
+        })
+}
+
+
+document.querySelector("#book-tour").addEventListener('click', e => {
+    e.target.textContent = 'Processing...';
+    const { tourId } = e.target.dataset;
+    bookTour(tourId);
+
+})
