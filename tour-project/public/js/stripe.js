18eb417dcf024a2206be2b903b0ee7e8500ec161
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-11-17 20:10:35 +0600

    stripe client and server payment process done

--- tour-project/public/js/stripe.js
+++ tour-project/public/js/stripe.js
@@ -1,6 +1,8 @@
-const stripe = Stripe('pk_test_51IeLQQJvn90Urdz7TTW8LBbHX1k1LUFWgveadGfsJKGdiI95E1m6DKTJW9NjGgSM58J5mtzhq3FbvTs9PzLFI0nk00j42bLZfM');
+
 
 const bookTour = tourId => {
+    const stripe = Stripe('pk_test_51IeLQQJvn90Urdz7TTW8LBbHX1k1LUFWgveadGfsJKGdiI95E1m6DKTJW9NjGgSM58J5mtzhq3FbvTs9PzLFI0nk00j42bLZfM');
+
     fetch(`http://localhost:8080/api/v1/bookings/checkout-session/${tourId}`)
         .then(res => res.json())
         .then(data => {
