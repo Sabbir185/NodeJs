fc6ff732af7cf99a2cd623fc8866b67980a715d0
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-11-20 22:54:24 +0600

    mongodb database connection and create some data

--- Nodejs_Testing/index.js
+++ Nodejs_Testing/index.js
@@ -1,1 +1,30 @@
-console.log('Hello nodejs')
+const express = require('express')
+const User = require('./models/User')
+const mongoose = require('mongoose')
+
+const app = express();
+
+app.use(express.json())
+
+
+// mongodb connection
+const db = 'mongodb://localhost/percelkoi';
+mongoose.connect(db).then(()=> console.log('Database connection success!')).catch(err=> console.log(err))
+
+app.get('/', (req, res) => {
+    const name = req.query.name;
+    res.send('Hello '+name)
+});
+
+app.post('/', async (req, res) => {
+    const username = req.body;
+    const newUser = new User( username );
+    await newUser.save().then(result => res.json(result)).catch(err => console.log(err))
+    // res.send('New user : '+ username);
+})
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log('Port is listening ',port)
+});
